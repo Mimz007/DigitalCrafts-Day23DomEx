38ba658f22b7e2ca93d5f2b3c869c1d3f2729e94
Amy Lazos <66265098+Mimz007@users.noreply.github.com>
2020-06-24 23:18:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//
+//
+//let newSection = document.createElement('section')
+//
+//let newH2 =  document.createElement('h2')
+//newH2.innerText = title
+//
+//let newP = document.createElement('p')
+//let image = document.createElement('image')
+//image.setAttribute("src", "")
+//newP.append(image)
+//newP.append(decription)
+//
+//let newP2 = document.createElement('p')
+//let newUl = document.createElement('ul')
+//let newLi = document.createElement('li')
+//newUl.append(newLi)
+//new.append(hobbies)
+//
+//let newP3 = document.createElement('p')
+//let newL2 = document.createElement('li')
+//newP3.append(newLi)
+//newL2.append(favBooks)
+//
+//newSection.append(newH2, newP, newP2, newP3)
+//
+//newP2.setAttribute("class", "paragraph")
+//newLi.setAttribute("class", "list")
+//newL2.setAttribute("class", "list")
+//newH2.setAttribute("class", "heading")
+//newP.setAttribute("class", "paragraph")
+//newP3.setAttribute("class", "paragraph")
+//
